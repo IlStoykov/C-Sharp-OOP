7c4e18d30aa8a4b971a8f18cc5df4000081a7804
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-09-04 19:53:36 +0300

    change namespace

--- CarDealership/Models/Vehicle.cs
+++ CarDealership/Models/Vehicle.cs
@@ -1,9 +1,5 @@
 ﻿using CarDealership.Models.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace CarDealership.Models
 {
