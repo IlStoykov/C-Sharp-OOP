bf8a69ad399e6c3790bbc303a1c4905d84295a81
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-09-04 20:53:39 +0300

    repositories

---
+++ CarDealership/Repositories/VehicleRepository.cs
@@ -0,0 +1,42 @@
+﻿using CarDealership.Models.Contracts;
+using CarDealership.Repositories.Contracts;
+
+
+namespace CarDealership.Repositories
+{
+    public class VehicleRepository : IRepository<IVehicle>
+    {
+        private List<IVehicle> vehicles;
+
+        public VehicleRepository() { 
+            vehicles = new List<IVehicle>();
+        }
+        public IReadOnlyCollection<IVehicle> Models => vehicles.AsReadOnly();
+
+        public void Add(IVehicle model)
+        {
+            if (!vehicles.Contains(model)) { 
+                vehicles.Add(model);
+            }
+        }
+        public bool Exists(string text)
+        {
+            return vehicles.Any(x => x.Model == text);
+        }
+
+        public IVehicle Get(string text)
+        {
+            return vehicles.FirstOrDefault(x => x.Model == text);
+        }
+
+        public bool Remove(string text)
+        {
+            IVehicle vehicle = vehicles.FirstOrDefault(y => y.Model == text);
+            if (vehicle != null) {
+                return false;
+            }
+            vehicles.Remove(vehicle);
+            return true;    
+        }
+    }
+}
