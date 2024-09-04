8bc71fbb68b0a2c96b7f805534a72fd73ab83633
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-09-04 20:10:45 +0300

    class Truck and abstract Customer

---
+++ CarDealership/Models/Customer.cs
@@ -0,0 +1,34 @@
+﻿using CarDealership.Models.Contracts;
+
+namespace CarDealership.Models
+{
+    public abstract class Customer : ICustomer
+    {
+        private string name;
+        private List<string> purchases;
+        public Customer(string name) { 
+            Name = name;
+            purchases = new List<string>();
+        }
+        public string Name {
+            get => name;
+            set {
+                if (string.IsNullOrWhiteSpace(value)) { 
+                    throw new ArgumentNullException("Name is required.");
+                }
+                name = value;
+            }
+        }
+        public IReadOnlyCollection<string> Purchases => purchases.AsReadOnly();
+
+        public void BuyVehicle(string vehicleModel)
+        {
+            if (!string.IsNullOrEmpty(vehicleModel)) {
+                purchases.Add(vehicleModel);
+            }
+        }
+        public override string ToString() => $"{Name} - Purchases: {Purchases.Count}";
+
+
+    }
+}
---
+++ CarDealership/Models/Truck.cs
@@ -0,0 +1,11 @@
+﻿
+namespace CarDealership.Models
+{
+    public class Truck : Vehicle
+    {
+        private const double IncreaseValue = 1.3;
+        public Truck(string model, double price) : base(model, price * IncreaseValue)
+        {
+        }
+    }
+}
