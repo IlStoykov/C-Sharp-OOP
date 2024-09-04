ee6dab818f20371f7916a83ccc7e3711eec29189
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-09-04 21:25:47 +0300

    work over controller

---
+++ CarDealership/Core/Controller.cs
@@ -0,0 +1,41 @@
+﻿using CarDealership.Core.Contracts;
+using CarDealership.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealership.Core
+{
+    public class Controller : IController
+    {
+        string[] customerType = new[] { "IndividualClient", "LegalEntityCustomer" };
+        public string AddCustomer(string customerTypeName, string customerName)
+        {
+            if (!customerType.Contains(customerTypeName)) {
+                return string.Format(OutputMessages.InvalidType, customerTypeName);
+            }
+        }
+
+        public string AddVehicle(string vehicleTypeName, string model, double price)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CustomerReport()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string PurchaseVehicle(string vehicleTypeName, string customerName, double budget)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SalesReport(string vehicleTypeName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CarDealership/Models/Dealership.cs
+++ CarDealership/Models/Dealership.cs
@@ -1,11 +1,7 @@
 ﻿using CarDealership.Models.Contracts;
 using CarDealership.Repositories;
 using CarDealership.Repositories.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace CarDealership.Models
 {
