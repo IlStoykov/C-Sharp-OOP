d497df56abf7a341b372890b8f6d69d8262f7472
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-09-21 21:39:59 +0300

    Update CargoVan.cs

--- EDriveRent/Models/CargoVan.cs
+++ EDriveRent/Models/CargoVan.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EDriveRent.Models
 {
     public class CargoVan : Vehicle
     {
         private const double constMileag = 180;
-        public CargoVan(string brand, string model, string licensePlateNumber) : base(brand, model, licensePlateNumber, constMileag)
+        public CargoVan(string brand, string model, string licensePlateNumber) 
+            : base(brand, model, constMileag, licensePlateNumber)
         {
         }
     }
