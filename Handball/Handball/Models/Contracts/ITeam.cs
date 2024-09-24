1b527ad4b9dab8e9feb9709299ebd96144e68ef9
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-09-24 20:40:07 +0300

    Update ITeam.cs

--- Handball/Handball/Models/Contracts/ITeam.cs
+++ Handball/Handball/Models/Contracts/ITeam.cs
@@ -1,1 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Handball.Models.Contracts
+{
+    public interface ITeam
+    {
+        public string Name { get; }
+        public int PointsEarned { get; }
+        public double OverallRating { get; }
+        public IReadOnlyCollection<IPlayer> Players { get; }
+        void SignContract(IPlayer player);
+
+        void Win();
+        void Lose();
+        void Draw();
+    }
+}
 
