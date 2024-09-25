37b4e9bc8b9f20234ebeb288a746a253b9397532
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-09-25 21:01:24 +0300

    Add files via upload

---
+++ Handball/Handball/Models/Goalkeeper.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Handball.Models
+{
+    public class Goalkeeper : Player
+    {
+        private const double intRating = 2.5;
+        public Goalkeeper(string name) : base(name, intRating)
+        {
+        }
+        public override void IncreaseRating() {
+            Rating += 0.75;
+        }
+        public override void DecreaseRating(){
+            Rating -= 1.25;
+        }
+    }
+}
