56979fad3d0f396e9fcad3797143302fa1350148
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-09-26 21:32:05 +0300

    Add files via upload

---
+++ Handball/Handball/Repositories/PlayerRepository.cs
@@ -0,0 +1,47 @@
+﻿using Handball.Models.Contracts;
+using Handball.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handball.Repositories
+{
+    public class PlayerRepository : IRepository<IPlayer>
+    {
+        private List<IPlayer> players;
+        public PlayerRepository() {     
+            players = new List<IPlayer>();
+        }
+        public IReadOnlyCollection<IPlayer> Models => players.AsReadOnly();
+
+        public void AddModel(IPlayer model)
+        {
+            IPlayer playerFpund = players.FirstOrDefault(x => x.Name == model.Name);
+            if (playerFpund == null){
+                players.Add(playerFpund);
+            }
+        }
+
+        public bool ExistsModel(string name)
+        {
+            return (players.Any(x => x.Name == name));
+        }
+
+        public IPlayer GetModel(string name) => players.Find(x => x.Name == name);
+        
+
+        public bool RemoveModel(string name)
+        {
+            IPlayer playerFound = players.FirstOrDefault(x => x.Name == name);
+            if (playerFound != null)
+            {
+                players.Remove(playerFound);
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
