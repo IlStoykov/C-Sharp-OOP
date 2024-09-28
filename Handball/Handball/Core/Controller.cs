06873a186f77bbe5e0c4bb7ebbef525b2626e0fb
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-09-28 21:19:01 +0300

    Update Controller.cs

--- Handball/Handball/Core/Controller.cs
+++ Handball/Handball/Core/Controller.cs
@@ -1,14 +1,25 @@
-﻿using Handball.Core.Contracts;
-using System;
-using System.Collections.Generic;
+using Handball.Core.Contracts;
+using Handball.Models;
+using Handball.Models.Contracts;
+using Handball.Repositories;
+using Handball.Utilities.Messages;using System;
+
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Handball.Core
 {
     public class Controller : IController
     {
+        private PlayerRepository players;
+        private TeamRepository teams;
+        private string [] playerTypeCheck = new string[] { "Goalkeeper", "CenterBack", "ForwardWing"};
+        public Controller(){
+            players = new PlayerRepository();
+            teams = new TeamRepository();
+        }
+        ITeam teamFound = null;
+        IPlayer playerFound = null;
         public string LeagueStandings()
         {
             throw new NotImplementedException();
@@ -16,7 +27,25 @@ namespace Handball.Core
 
         public string NewContract(string playerName, string teamName)
         {
-            throw new NotImplementedException();
+            teamFound = teams.GetModel(teamName);
+            playerFound = players.GetModel(playerName);
+
+            if (playerFound == null)
+            {
+                return string.Format(OutputMessages.PlayerNotExisting, playerName, nameof(players));
+            }
+            else if (teamFound == null)
+            {
+                return string.Format(OutputMessages.TeamNotExisting, teamName, nameof(teams));
+            }
+            else if (playerFound.Team != null)
+            {
+                return string.Format(OutputMessages.PlayerAlreadySignedContract, playerName, playerFound.Team);
+            }            
+                
+            playerFound.JoinTeam(teamName);
+            teamFound.SignContract(playerFound);
+            return string.Format(OutputMessages.SignContract, playerName, teamName);            
         }
 
         public string NewGame(string firstTeamName, string secondTeamName)
@@ -26,14 +55,42 @@ namespace Handball.Core
 
         public string NewPlayer(string typeName, string name)
         {
-            throw new NotImplementedException();
+            if (!playerTypeCheck.Contains(typeName)) {
+                return string.Format(OutputMessages.InvalidTypeOfPosition, typeName);
+            }
+            playerFound = players.GetModel(name);
+            if (playerFound != null)
+            {
+                return string.Format(OutputMessages.PlayerIsAlreadyAdded, name, players.GetType().Name, typeName);
+            }
+            if (typeName == "Goalkeeper")
+            {
+               playerFound = new Goalkeeper(name);
+            }
+            if (typeName == "ForwardWing")
+            {
+                playerFound = new ForwardWing(name);
+            }
+            if (typeName == "CenterBack") { 
+                playerFound = new CenterBack(name);
+            }
+            players.AddModel(playerFound);
+            return string.Format(OutputMessages.PlayerAddedSuccessfully, name);
         }
 
         public string NewTeam(string name)
         {
-            throw new NotImplementedException();
+            teamFound = teams.GetModel(name);
+            if (teamFound != null)
+            {
+                return string.Format(OutputMessages.TeamAlreadyExists, name, teams.GetType().Name);
+            }
+            else {
+                teamFound = new Team(name);
+                teams.AddModel(teamFound);
+                return string.Format(OutputMessages.TeamSuccessfullyAdded, name, teams.GetType().Name);
+            }            
         }
-
         public string PlayerStatistics(string teamName)
         {
             throw new NotImplementedException();
