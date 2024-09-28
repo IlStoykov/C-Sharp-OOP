1df4d289cb34e7c29aa0938f06c873f96886e534
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-09-28 12:38:41 +0300

    Add files via upload

---
+++ Handball/Handball/Core/Controller.cs
@@ -0,0 +1,42 @@
+﻿using Handball.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handball.Core
+{
+    public class Controller : IController
+    {
+        public string LeagueStandings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string NewContract(string playerName, string teamName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string NewGame(string firstTeamName, string secondTeamName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string NewPlayer(string typeName, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string NewTeam(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string PlayerStatistics(string teamName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
