be475184b51056271541edcd96b7c037527f96de
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-09-30 21:43:45 +0300

    Update UnitTest1.cs

--- SmartDevice/SmartDevice.Tests/UnitTest1.cs
+++ SmartDevice/SmartDevice.Tests/UnitTest1.cs
@@ -6,15 +6,17 @@ namespace SmartDevice.Tests
 
     public class Tests
     {
+        Device testDevice = new Device(500);
+
         [SetUp]
         public void Setup()
-        {
+        {           
         }
 
         [Test]
         public void Test_DeviceConstructorWorkPropper() 
         {
-            Device testDevice = new Device(500);
+            
             int expectResult = 500;
             var propertyValue = testDevice.Applications;
             
@@ -22,6 +24,31 @@ namespace SmartDevice.Tests
             Assert.AreEqual(testDevice.MemoryCapacity, expectResult);
             Assert.IsTrue(testDevice.Photos == 0);
             Assert.That(propertyValue, Is.InstanceOf<List<string>>());
+        }
+        [Test]
+        public void Test_DeviceTakePhotoMemorySetProper() {
+            int testResultGraeterMemory = 1000;
+            int testResultLowerMemory = 200;
+
+            Assert.IsFalse(testDevice.TakePhoto(testResultGraeterMemory));
+            Assert.IsTrue(testDevice.TakePhoto(testResultLowerMemory));
+        }
+        [Test]
+        public void Test_DeviceTakePhotoCountProper() {
+            int expectResult = 1;
+            testDevice.TakePhoto(100);
+
+            Assert.AreEqual(expectResult, testDevice.Photos);
+        }
+        [Test]
+        public void Test_DevicetakePhotosMemoryWorkProper() {
+            int memoryUsage = 100;
+            int expectResult = testDevice.MemoryCapacity - memoryUsage;
+            
+            testDevice.TakePhoto(memoryUsage);
+
+            Assert.AreEqual(testDevice.AvailableMemory, expectResult);            
+
         }
     }
 }
