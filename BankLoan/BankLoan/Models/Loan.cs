68d7d1dee85c939a0aa51fbcd589fdf5348394ea
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-10-03 21:35:08 +0300

    Update Loan.cs

--- BankLoan/BankLoan/Models/Loan.cs
+++ BankLoan/BankLoan/Models/Loan.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BankLoan.Models.Contracts;
+
 
 namespace BankLoan.Models
 {
-    public abstract class Loan
+    public abstract class Loan : ILoan
     {
         public Loan(int interestRate, double amount)
         {
