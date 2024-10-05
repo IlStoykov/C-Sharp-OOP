c7f1e73f7446e640bf182bd8bcc85d283fbbb240
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-10-05 17:49:40 +0300

    Add files via upload

---
+++ BankLoan/BankLoan/Repositories/LoanRepository.cs
@@ -0,0 +1,42 @@
+﻿using BankLoan.Models.Contracts;
+using BankLoan.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace BankLoan.Repositories
+{
+    public class LoanRepository : IRepository<ILoan>
+    {
+        private List<ILoan> loans;
+        ILoan modelFound = null;
+        public LoanRepository() {
+            loans = new List<ILoan>();
+        }
+        public IReadOnlyCollection<ILoan> Models => loans.AsReadOnly();
+
+        public void AddModel(ILoan model)
+        {
+            modelFound = loans.FirstOrDefault(x => x.Equals(model));
+            if (modelFound == null) {
+                loans.Add(model);
+            }
+        }
+        public ILoan FirstModel(string name)
+        {
+            return loans.FirstOrDefault(x => x.GetType().Name == name);
+        }
+
+        public bool RemoveModel(ILoan model)
+        {
+            modelFound = loans.FirstOrDefault(x => x.Equals(model));
+            if (modelFound != null)
+            {
+                loans.Remove(model);
+                return true;
+            }
+            return false;
+        }
+    }
+}
