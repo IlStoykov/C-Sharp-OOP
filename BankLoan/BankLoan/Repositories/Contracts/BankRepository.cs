ff953d7ee16e475f30e99e92ae729c84f384b3d8
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-10-05 17:33:10 +0300

    Add files via upload

---
+++ BankLoan/BankLoan/Repositories/Contracts/BankRepository.cs
@@ -0,0 +1,39 @@
+﻿using BankLoan.Models.Contracts;
+using BankLoan.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace BankLoan.Repositories
+{
+    public class BankRepository : IRepository<IBank>
+    {
+        List<IBank> banks;
+       
+        BankRepository() { 
+            banks = new List<IBank>();
+        }
+        public IReadOnlyCollection<IBank> Models => banks.AsReadOnly();
+
+        public void AddModel(IBank model)
+        {
+            if (!banks.Any(x => x.Name == model.Name)){
+                banks.Add(model);
+            }
+        }
+        public IBank FirstModel(string name)
+        {
+            return banks.FirstOrDefault(x => x.Name == name);
+        }
+
+        public bool RemoveModel(IBank model)
+        {
+            if (banks.Any(x => x.Name == model.Name)) { 
+                banks.Remove(model);
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ BankLoan/BankLoan/Repositories/Contracts/LoanRepository.cs
@@ -0,0 +1,43 @@
+﻿using BankLoan.Models.Contracts;
+using BankLoan.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLoan.Repositories
+{
+    public class LoanRepository : IRepository<ILoan>
+    {
+        private List<ILoan> loans;
+        ILoan modelFound = null;
+        LoanRepository() {
+            loans = new List<ILoan>();
+        }
+        public IReadOnlyCollection<ILoan> Models => loans.AsReadOnly();
+
+        public void AddModel(ILoan model)
+        {
+            modelFound = loans.FirstOrDefault(x => x.Amount == model.Amount && x.InterestRate == model.InterestRate);
+            if (modelFound == null) {
+                loans.Add(model);
+            }
+        }
+        public ILoan FirstModel(string name)
+        {
+            return loans.First(x => x.GetType().Name == name);
+        }
+
+        public bool RemoveModel(ILoan model)
+        {
+            modelFound = loans.FirstOrDefault(x => x.Amount == model.Amount && x.InterestRate == model.InterestRate);
+            if (modelFound != null)
+            {
+                loans.Remove(model);
+                return true;
+            }
+            return false;
+        }
+    }
+}
