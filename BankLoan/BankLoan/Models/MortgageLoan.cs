56f2d2b41e31733aaade148e654217e87f4deea7
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-10-06 21:56:44 +0300

    Update MortgageLoan.cs

--- BankLoan/BankLoan/Models/MortgageLoan.cs
+++ BankLoan/BankLoan/Models/MortgageLoan.cs
@@ -3,7 +3,7 @@ namespace BankLoan.Models
     public class MortgageLoan : Loan
     {
         private const int FixedInterestRate = 1;
-        private const double FixedAmount = 10000.0;
+        private const double FixedAmount = 50000.0;
         public MortgageLoan() : base(FixedInterestRate, FixedAmount)
         {
         }
