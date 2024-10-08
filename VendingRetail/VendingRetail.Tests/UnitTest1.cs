a4bc561fc2777ddb7d89cae73d68ef4fb5280204
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-10-08 22:01:34 +0300

    Update UnitTest1.cs

--- VendingRetail/VendingRetail.Tests/UnitTest1.cs
+++ VendingRetail/VendingRetail.Tests/UnitTest1.cs
@@ -3,18 +3,33 @@ using NUnit.Framework;
 namespace VendingRetail.Tests
 {
     public class Tests
-    {
-        
+    {        
+        CoffeeMat TestCoffeeMat = null;
+        int testWaterCapacity = 500;
+        int testButtonsCount = 5;
         [SetUp]
         public void Setup()
-        {
-            
+        {            
         }
 
         [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }        
+        public void Test_SetCoffeeMatConstructorProper() {
+            
+
+            TestCoffeeMat = new CoffeeMat(testWaterCapacity, testButtonsCount);
+
+            Assert.AreEqual(TestCoffeeMat.WaterCapacity, testWaterCapacity);
+            Assert.AreEqual(TestCoffeeMat.ButtonsCount, testButtonsCount);
+            Assert.AreEqual(TestCoffeeMat.Income, 0);           
+        }
+        [Test]
+        public void Test_FillWaterTankIfFull() {
+            string expectedResult = "Water tank is already full!";
+            
+            TestCoffeeMat = new CoffeeMat(testWaterCapacity, testButtonsCount);
+            string tankFullMessage = TestCoffeeMat.FillWaterTank();
+            
+            Assert.AreEqual(tankFullMessage, expectedResult);
+        }
     }
 }
