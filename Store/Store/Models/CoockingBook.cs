7b37a4a281a0cafeaf556e4a836099a86a5c9e1e
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-10-14 20:45:07 +0300

    Add files via upload

---
+++ Store/Store/Models/CoockingBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Models
+{
+    public class CoockingBook : Book
+    {
+        private const string BookType = "CookingBook";
+        public CoockingBook(string author, string title, double price) : base(author, title, price, BookType)
+        {
+        }
+    }
+}
---
+++ Store/Store/Models/NovelBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Models
+{
+    public class NovelBook : Book
+    {
+        private const string BookType = "NovelBook";
+        public NovelBook(string author, string title, double price, string genre) : base(author, title, price, BookType)
+        {
+        }
+    }
+}
