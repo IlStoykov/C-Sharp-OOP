e7ebcb2e3bacfdf7e22de219abc1aaee955b47a4
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-10-14 20:11:13 +0300

    Update Book.cs

--- Store/Store/Models/Book.cs
+++ Store/Store/Models/Book.cs
@@ -1,10 +1,6 @@
 ﻿using Store.Models.Contracts;
 using Store.Utilities.Messages;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Store.Models
 {
