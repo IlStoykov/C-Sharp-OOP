6ab4ea0966d54550eb23be5abf5b74a88f8f71ae
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-10-14 20:09:49 +0300

    Add files via upload

---
+++ Store/Store/Models/Book.cs
@@ -0,0 +1,29 @@
+﻿using Store.Models.Contracts;
+using Store.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Models
+{
+    public abstract class Book : IBook
+    {
+        private double price;
+        public Book() { }
+        public string Author { get; private set; }
+
+        public string Title { get; private set; }
+
+        public double Price {
+            get => price;
+            private set {
+                if (value <= 0) {
+                    throw new ArgumentException(ExceptionMessages.PriceMustBePositive);
+                }
+            }
+        }
+        public string Genre { get; private set; }
+    }
+}
---
+++ Store/Store/Models/Contracts/IBook.cs
@@ -0,0 +1,10 @@
+﻿namespace Store.Models.Contracts
+{
+    public interface IBook
+    {
+        string Author { get; }
+        string Title { get; }
+        double Price { get; }
+        string Genre { get; }
+    }
+}
