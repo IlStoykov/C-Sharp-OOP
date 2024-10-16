ac8cbee3e545bc970bfd441272d986f778432aee
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-10-16 21:33:12 +0300

    Add files via upload

---
+++ Store/Store/Models/Contracts/IStore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotService.Models.Contracts
+{
+    public interface IStore<T> where T : class
+    {
+        const int WhereHouseMaxLimit = 10;
+        const int WhereHouseMinLimit = 3;       
+
+
+        string StoreType { get; }
+        List<T> WareHouse { get; } 
+        double Turnover { get; }
+        double Profit { get; }
+        Dictionary<string, double> ProfitTable { get; }
+
+        void Order(string kind);
+    }
+}
