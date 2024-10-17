3610f1e1d70f9e3e879ad6622aeac0b1979fca74
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-10-17 22:22:26 +0300

    Update Store.cs

--- Store/Store/Models/Store.cs
+++ Store/Store/Models/Store.cs
@@ -1,38 +1,75 @@
-﻿using RobotService.Models.Contracts;
+using RobotService.Models.Contracts;
+using Store.Models.Contracts;
 using Store.Utilities.Messages;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Store.Models
 {
-    public class Store<T>(string storeKind) : IStore<T> where T : class
+    public class Store<T> : IStore<T> where T : class, IOfficeSupplies
     {
         private string storeType;
         private string[] storeTypes = new string[] { "BookStore", "OfficeStore" };
+        private List<T> storeWarehouse;
+        private Dictionary<string, double> profitTable = 
+            new Dictionary<string, double>() { { "Pen", 1.05 }, { "Pencil", 2.45}, { "NovelBook", 4.55}, { "CoockingBook", 6.85} };
+
+        public Store(string storeType)
+        {
+            this.storeType = storeType;
+            storeWarehouse = new List<T>();
+        }
         public string StoreType { 
             get => storeType;
             set {
                 if (!storeTypes.Contains(value)) {
                     throw new ArgumentException(ExceptionMessages.StoreType);
                 }
-            }
-            
+                storeType = value;
+            }         
         }
+        public List<T> WareHouse => storeWarehouse;
 
-        public List<T> WareHouse => throw new NotImplementedException();
+        public double Turnover { get; private set; }
 
-        public double Turnover => throw new NotImplementedException();
+        public double Profit { get; private set; }
 
-        public double Profit => throw new NotImplementedException();
-
-        public Dictionary<string, double> ProfitTable => throw new NotImplementedException();
-
-        public void Order(string kind)
+        public string Order(string kind)
         {
-            throw new NotImplementedException();
+            IOfficeSupplies suppliceFound = storeWarehouse.FirstOrDefault(x => x.GetType().Name == kind);
+            switch (kind)
+            {
+                case "Pen":                    
+                    if (suppliceFound == null)
+                    {
+                        throw new ArgumentException(string.Format(ExceptionMessages.OutOfStock, kind));
+                    }
+                    break;
+                case "Pencil":                    
+                    if (suppliceFound == null)
+                    {
+                        throw new ArgumentException(string.Format(ExceptionMessages.OutOfStock, kind));
+                    }
+                    break;
+                case "NovelBook":                   
+                    if (suppliceFound == null)
+                    {
+                        throw new ArgumentException(string.Format(ExceptionMessages.OutOfStock, kind));
+                    }
+                    break;
+                case "CoockingBook":                    
+                    if (suppliceFound == null)
+                    {
+                        throw new ArgumentException(string.Format(ExceptionMessages.OutOfStock, kind));
+                    }
+                    break;
+            }
+            double profitAdding = profitTable[kind];
+            Profit += profitAdding;
+            Turnover += (profitAdding + suppliceFound.Price);
+            WareHouse.Remove((T)suppliceFound);
+            return $"A {suppliceFound.GetType().Name} was sold on a price of {(profitAdding + suppliceFound.Price)}";
+
         }
     }
+    
 }
