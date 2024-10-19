24cbb79df5220b54bb45f88c987a311f28ba1cd8
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-10-19 21:19:52 +0300

    Add files via upload

---
+++ RobotService/Repositories/RobotRepository.cs
@@ -0,0 +1,39 @@
+﻿using RobotService.Models.Contracts;
+using RobotService.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotService.Repositories
+{
+    public class RobotRepository : IRepository<IRobot>
+    {
+        private List<IRobot> robots;
+        IRobot robotFound = null;
+        public RobotRepository() { 
+            robots = new List<IRobot>();
+        }
+        public void AddNew(IRobot model)
+        {
+            if (robots.FirstOrDefault(x => x.Model == model.Model) == null) { 
+                robots.Add(model);
+            }
+        }
+        public IRobot FindByStandard(int interfaceStandard) {
+            return robots.FirstOrDefault(x => x.InterfaceStandards.Any(x => x == interfaceStandard));
+        }        
+        public IReadOnlyCollection<IRobot> Models() => robots.AsReadOnly();        
+
+        public bool RemoveByName(string typeName)
+        {
+            robotFound = robots.FirstOrDefault(x => x.Model == typeName);
+            if (robotFound != null) { 
+                robots.Remove(robotFound);
+                return true;
+            }
+            return false;
+        }
+    }
+}
