1f346498c0638f888d9917c0fd2db21cbbf5f3ad
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-10-22 21:56:42 +0300

    Add files via upload

---
+++ Store/Store/GeneralWarehouse/Contracts/IGeneralWarehouse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.GeneralWarehouse.Contracts
+{
+    public interface IGeneralWarehouse<T>
+    {
+        public IReadOnlyCollection<T> Warehouse();
+        void Add(T item);
+        public List<T> ProduceDelivery(string storeType, int number);
+
+
+
+    }
+}
