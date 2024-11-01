8f873cf99414361f5c31dd72cf48d908310ded83
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-11-01 21:13:42 +0200

    Update IStore.cs

--- Store/Store/Models/Contracts/IStore.cs
+++ Store/Store/Models/Contracts/IStore.cs
@@ -1,13 +1,13 @@
 
 namespace RobotService.Models.Contracts
 {
-    public interface IStore<T> where T : class
+    public interface IStore
     {
         const int WhereHouseMaxLimit = 10;
         const int WhereHouseMinLimit = 3;       
 
         string StoreType { get; }
-        List<T> WareHouse { get; } 
+         List<object> WareHouse { get; }  
         double Turnover { get; }
         double Profit { get; }   
 
