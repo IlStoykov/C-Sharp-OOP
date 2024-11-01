c7ddfd850283bdb4f306843ef9e63ce30f69612f
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-11-01 21:47:26 +0200

    Update Store.cs

--- Store/Store/Models/Store.cs
+++ Store/Store/Models/Store.cs
@@ -1,22 +1,25 @@
-using RobotService.Models.Contracts;
 using Store.Models.Contracts;
 using Store.Utilities.Messages;
-
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Store.Models
 {
     public class Store<T> : IStore<T> where T : class, IOfficeSupplies
-    {
+    {        
         private int orderCount = 0;
         private string storeType;
+        private string storeName;
         private string[] storeTypes = new string[] { "BookStore", "OfficeStore" };
         private List<T> storeWarehouse;
         private Dictionary<string, double> profitTable = 
             new Dictionary<string, double>() { { "Pen", 1.05 }, { "Pencil", 2.45}, { "NovelBook", 4.55}, { "CoockingBook", 6.85} };
 
-        public Store(string storeType)
+        public Store(string storeType, string name)
         {
             this.storeType = storeType;
+            this.storeName = name;
             storeWarehouse = new List<T>();
         }
         public string StoreType { 
@@ -28,6 +31,15 @@ namespace Store.Models
                 storeType = value;
             }         
         }
+        public string StoreName{
+            get => storeName;
+            private set {
+                if (string.IsNullOrWhiteSpace(value)) {
+                    throw new ArgumentException(ExceptionMessages.StoreName);
+                }
+                storeName = value;
+            }
+        }
         public List<T> WareHouse => storeWarehouse;
 
         public double Turnover { get; private set; }
@@ -38,34 +50,7 @@ namespace Store.Models
         {
             IOfficeSupplies suppliceFound = storeWarehouse.FirstOrDefault(x => x.GetType().Name == kind);
             CheckNullAndThrow(suppliceFound, kind);
-
-            //switch (kind)
-            //{
-            //    case "Pen":                    
-            //        if (suppliceFound == null)
-            //        {
-            //            throw new ArgumentException(string.Format(ExceptionMessages.OutOfStock, kind));
-            //        }
-            //        break;
-            //    case "Pencil":                    
-            //        if (suppliceFound == null)
-            //        {
-            //            throw new ArgumentException(string.Format(ExceptionMessages.OutOfStock, kind));
-            //        }
-            //        break;
-            //    case "NovelBook":                   
-            //        if (suppliceFound == null)
-            //        {
-            //            throw new ArgumentException(string.Format(ExceptionMessages.OutOfStock, kind));
-            //        }
-            //        break;
-            //    case "CoockingBook":                    
-            //        if (suppliceFound == null)
-            //        {
-            //            throw new ArgumentException(string.Format(ExceptionMessages.OutOfStock, kind));
-            //        }
-            //        break;
-            //}
+            
             double profitAdding = profitTable[kind];
             double totalIncome = suppliceFound.Price + profitAdding;
             
@@ -74,11 +59,9 @@ namespace Store.Models
             WareHouse.Remove((T)suppliceFound);
             orderCount ++;
             return $"A {suppliceFound.GetType().Name} was sold on a price of {totalIncome}";
-        }
-        public void StockLoading() {
-
-            return;
-        }
+        }        
+        
+        
         private void CheckNullAndThrow(IOfficeSupplies suppliceFound, string kind) {
             if (suppliceFound == null)
             {
