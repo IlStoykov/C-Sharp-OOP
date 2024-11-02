ae3092c611b5ea9ec60a2aaf8e834c179dbcc25e
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-11-02 22:38:54 +0200

    Update ExceptionMessages.cs

--- Store/Store/Utilities/Messages/ExceptionMessages.cs
+++ Store/Store/Utilities/Messages/ExceptionMessages.cs
@@ -1,8 +1,12 @@
-﻿
+
 namespace Store.Utilities.Messages
 {
     public class ExceptionMessages
     {
         public const string PriceMustBePositive = "The price should be bigger than zero!";
+        public const string StoreType = "The type of the store must be BookStore, OfficeStore.";
+        public const string StoreName = "The name of the store could not be null or white space.";
+        public const string OutOfStock = "The store {0}, type of {1} is out of stock";
+        
     }
 }
