1cebb9c3b1c4891aa25e28b99b31ced02660fec9
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-11-04 09:55:36 +0200

    Update IStore.cs

--- Store/Store/Models/Contracts/IStore.cs
+++ Store/Store/Models/Contracts/IStore.cs
@@ -1,16 +1,16 @@
-﻿
+
 namespace Store.Models.Contracts
 {
     public interface IStore<T> where T : class
     {
-        const int WhereHouseMaxLimit = 10;
-        const int WhereHouseMinLimit = 3;   
-                
-        string StoreName { get; }
+                       
+        string StoreName { get; set; }
         List<T>WareHouse { get; } 
         double Turnover { get; }
         double Profit { get; }   
 
-        string Order(string kind);
-    }
+        string Order(string item);
+        void CheckWareHouseCapacity();
+        
+        }
 }
