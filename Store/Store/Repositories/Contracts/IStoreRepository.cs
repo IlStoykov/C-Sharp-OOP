cf23b1ade13b1dd4c07198bb0d65b6e3f8a5ea20
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-11-05 21:27:48 +0200

    Update IStoreRepository.cs

--- Store/Store/Repositories/Contracts/IStoreRepository.cs
+++ Store/Store/Repositories/Contracts/IStoreRepository.cs
@@ -7,7 +7,7 @@ namespace Store.Repositories.Contracts
     public interface IStoreRepository<TStore, T> where TStore : IStore<T> where T:class
     {
         ReadOnlyCollection<TStore> Models { get; }
-        string Add(TStore store);
+        void Add(TStore store);
         bool FindByName(string name);
     }
 }
