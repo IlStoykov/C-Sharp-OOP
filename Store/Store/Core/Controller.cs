6e5b8473d957a5fef6cec85532be1a844c106f67
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-11-05 21:43:58 +0200

    Update Controller.cs

--- Store/Store/Core/Controller.cs
+++ Store/Store/Core/Controller.cs
@@ -1,18 +1,32 @@
 using Store.Core.Contracts;
 using Store.GeneralWarehouse;
-
+using Store.Models;
+using Store.Utilities.Messages;
 
 namespace Store.Core
 {
     public class Controller : IController
-    {
+    {        
         private GWarehouse<object> generalWarehouse;
+        private string[] possibleStoreTypes = new string[] { "BookStore", "OfficeStore" };
         public Controller() {
             generalWarehouse = new GWarehouse<object>();            
         }
-        public string CreateStore(string storeType)
+        public string CreateStore(string storeType, string storeName)
         {
-            return storeType;
+            Store<object> newStore = null;
+            if (!possibleStoreTypes.Contains(storeType)) {
+                return string.Format(OutputMessages.InvalidStoreType);
+            }
+            if (storeType == "BookStore") {
+                newStore = new BookStore<Book>(storeName); 
+            }
+            else if (storeType == "OfficeStore") {
+                newStore = new OfficeStore<OfficeSupplies>(storeName);
+            }
+            generalWarehouse.Add(newStore);
+            return String.Format(OutputMessages.StoreAdded, storeName);
+            
         }
     }
 }
