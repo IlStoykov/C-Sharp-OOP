89a735b0e0697911f35b512fe795e0ba8aeb4946
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-11-05 21:12:53 +0200

    Update IStoreRepository.cs

--- Store/Store/Repositories/Contracts/IStoreRepository.cs
+++ Store/Store/Repositories/Contracts/IStoreRepository.cs
@@ -1,17 +1,13 @@
-﻿using Store.Models.Contracts;
-using System;
-using System.Collections.Generic;
+using Store.Models.Contracts;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Store.Repositories.Contracts
 {
     public interface IStoreRepository<TStore, T> where TStore : IStore<T> where T:class
     {
-        ReadOnlyCollection<TStore> Stores();
-        void Add(TStore store);
-        void FindByName(string name);
+        ReadOnlyCollection<TStore> Models { get; }
+        string Add(TStore store);
+        bool FindByName(string name);
     }
 }
