98084940f12139eb2f591c59b0b3ef234255e0dd
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-11-06 21:58:53 +0200

    Update OfficeSupplies.cs

--- Store/Store/Models/OfficeSupplies.cs
+++ Store/Store/Models/OfficeSupplies.cs
@@ -4,31 +4,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-
 namespace Store.Models
 {
     public abstract class OfficeSupplies : IOfficeSupplies
     {        
-        private double price;               
+        private double price;
+        private string checkPackage;
 
-        public OfficeSupplies(string manufacturer, bool isPackage, double price, int productNumber )
+        public OfficeSupplies(string manufacturer, string package, double price, int productNumber )
         {           
-            Manufacturer = manufacturer;
-            IsPackage = isPackage;
+            Manufacturer = manufacturer;            
             Price = price;
             ProductNumber = productNumber;
-            
+            checkPackage = package;            
         }            
 
         public abstract string Color { get; }
         public string Manufacturer { get; private set; }
 
-        public bool IsPackage { get; private set; }
-
+       
         public double Price { 
             get => price;
             private set{
-                if (IsPackage) {
+                if (checkPackage == "yes") {
                     price = value * 10; 
                 }
                 price = value;
