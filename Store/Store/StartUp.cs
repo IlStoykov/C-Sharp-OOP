45a5df624467ccaa5dd8f5080b678a671d3f70b2
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-11-06 16:22:45 +0200

    Update StartUp.cs

--- Store/Store/StartUp.cs
+++ Store/Store/StartUp.cs
@@ -1,12 +1,16 @@
-﻿
+
+using Store.Core;
+using Store.Core.Contracts;
+
 namespace Store
 {
-    
+
     public class StartUp
     {
-        public static void Main(string[] args) {
-            IEngine engine = new IEngine();
+        static void Main(string[] args) {
+            IEngine engine = new Engine();
             engine.Run();
         }
     }
 }
+
