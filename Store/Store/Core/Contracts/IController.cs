8a3944a4a6778cac46263abd60baaa24efff7591
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-11-07 22:18:00 +0200

    Update IController.cs

--- Store/Store/Core/Contracts/IController.cs
+++ Store/Store/Core/Contracts/IController.cs
@@ -1,7 +1,8 @@
-﻿namespace Store.Core.Contracts
+namespace Store.Core.Contracts
 {
     public interface IController
     {
-        string CreateStore(string storeType);
+        string CreateStore(string storeType, string storeName);
+        string CreateProduct(string productType, string origin, string titleCount, double price, int productNumber);
     }
 }
