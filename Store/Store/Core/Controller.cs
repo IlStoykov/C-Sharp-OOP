994bb35792939f8114ab16b2bc402171ab1c0de5
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-11-08 22:29:03 +0200

    Update Controller.cs

--- Store/Store/Core/Controller.cs
+++ Store/Store/Core/Controller.cs
@@ -57,6 +57,7 @@ namespace Store.Core
                 }
             }
             generalWarehouse.Add(newProduct);
+            return String.Format(OutputMessages.ItemAdde, newProduct.GetType().Name, price, counter) + Environment.NewLine;
         }
     }
 }
