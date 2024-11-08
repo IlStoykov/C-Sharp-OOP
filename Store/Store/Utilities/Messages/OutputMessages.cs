2df1ad67e6e08d40634207689a584f8423e4a4fb
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-11-08 22:30:19 +0200

    Add files via upload

---
+++ Store/Store/Utilities/Messages/OutputMessages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Utilities.Messages
+{
+    public class OutputMessages
+    {
+        public const string InvalidStoreType = "The type of the store must be BookStore or OfficeStore";
+        public const string StoreExist = "Store wint name {0} already exist";
+        public const string StoreAdded = "Store with name {0} successfully added to the repository";
+        public const string ItemAdde = "Item {0} with delivery price {1} and product number {2} successfully addred to Genral Store.";
+    }
+}
