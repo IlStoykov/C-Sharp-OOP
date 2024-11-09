2761298c3be739367da149957c8122c0a9eef776
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-11-09 21:21:39 +0200

    Add files via upload

---
+++ Store/Store/Models/Contracts/IStore.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Store.Models.Contracts
+{
+    public interface IStore
+    {
+        int WareHouseMaxLimit { get; }
+        int WareHouseMinLimit { get; }
+        string StoreName { get; set; }        
+        double Turnover { get; }
+        double Profit { get; } 
+        string Order(string item);
+        int CheckWareHouseCapacity();
+        void AcceptDelivery(List<object> deliveryItems);
+        }
+    public interface IStore<T> : IStore where T : class {
+        List<T> WareHouse { get; }
+    }
+}
