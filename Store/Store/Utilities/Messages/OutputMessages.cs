394d61a48c135923a4e85176cb488afe4cb0a1ae
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-11-11 21:38:26 +0200

    Update OutputMessages.cs

--- Store/Store/Utilities/Messages/OutputMessages.cs
+++ Store/Store/Utilities/Messages/OutputMessages.cs
@@ -12,7 +12,7 @@ namespace Store.Utilities.Messages
         public const string StoreExist = "Store wint name {0} already exist";
         public const string StoreAdded = "Store with name {0} successfully added to the repository";
         public const string ItemAdde = "Item {0} with delivery price {1} and product number {2} successfully addred to Genral Store.";
-        public const string NoSuchStore = "Store with name {} does not exist in repository";
+        public const string NoSuchStore = "Store with name {0} does not exist in repository";
         public const string StoreDelivery = "The store type {0} with name {1} received delivery of {2} items";
     }
 }
