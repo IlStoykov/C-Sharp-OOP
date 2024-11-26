a420be67218e66a8fe8781970a672caf94257a2a
IlStoykov <134512873+IlStoykov@users.noreply.github.com>
2024-11-26 21:43:49 +0200

    Update Controller.cs

--- RobotService/Core/Controller.cs
+++ RobotService/Core/Controller.cs
@@ -5,16 +5,6 @@ using RobotService.Repositories;
 using RobotService.Utilities.Messages;
 using System;
 using System.Linq;
-
-namespace RobotService.Core
-{
-    public class Controller : using RobotService.Core.Contracts;
-using RobotService.Models;
-using RobotService.Models.Contracts;
-using RobotService.Repositories;
-using RobotService.Utilities.Messages;
-using System;
-using System.Linq;
 using System.Text;
 
 
@@ -63,7 +53,7 @@ namespace RobotService.Core
                     break;
 
                 case "LaserRadar":
-                    newSupplement = new SpecializedArm(typeName);
+                    newSupplement = new LaserRadar(typeName);
                     break;
             }
             supplements.AddNew (newSupplement);
@@ -144,61 +134,6 @@ namespace RobotService.Core
             supplements.RemoveByName(supplementTypeName);
 
             return string.Format(OutputMessages.UpgradeSuccessful, model, supplementTypeName);
-        }
-    }
-}
-
-    {
-        private SupplementRepository supplements;
-        private RobotRepository robots;
-        private string[] robotsType = new[] { "DomesticAssistant", "IndustrialAssistant" }; 
-        public Controller() { 
-            supplements = new SupplementRepository();
-            robots = new RobotRepository();
-        }
-        public string CreateRobot(string model, string typeName)
-        {
-            IRobot newRobot = null;
-            if (!robotsType.Contains(typeName)) { 
-                return string.Format(OutputMessages.RobotCannotBeCreated, typeName);
-            }
-            switch (typeName)
-            {
-                case "DomesticAssistant":
-                    newRobot = new DomesticAssistant(model);
-                    break;
-
-                case "IndustrialAssistant":
-                    newRobot = new IndustrialAssistant (model);
-                    break;
-            }
-            robots.AddNew(newRobot);
-            return string.Format(OutputMessages.RobotCreatedSuccessfully, typeName, model);
-        }
-
-        public string CreateSupplement(string typeName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string PerformService(string serviceName, int intefaceStandard, int totalPowerNeeded)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string Report()
-        {
-            throw new NotImplementedException();
-        }
-
-        public string RobotRecovery(string model, int minutes)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string UpgradeRobot(string model, string supplementTypeName)
-        {
-            throw new NotImplementedException();
         }
     }
 }
